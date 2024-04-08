64f56391147c584ec3ef55a45008470ac8d6c0d8
Muhammed Anees P P <muhammedaneespp.work@gmail.com>
2024-04-09 00:14:00 +0530

    Added login system and integrated Bootstrap forms and alerts for frontend

--- website/views.py
+++ website/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    #check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in, Please try again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    pass
+
