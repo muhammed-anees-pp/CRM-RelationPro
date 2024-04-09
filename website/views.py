3cb5e0e8db197a0b616509c5cada52862ee5b24a
Muhammed Anees P P <muhammedaneespp.work@gmail.com>
2024-04-09 21:46:19 +0530

    Register user is added with username, first name, last name and password

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from . forms import SignUpForm
 
 
 def home(request):
@@ -25,8 +26,29 @@ def home(request):
 # def login_user(request):
 #     pass
 
+#Logout function
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registred")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
+
 
