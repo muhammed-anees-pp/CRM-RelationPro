58dbb6019e1782036317856606af0a996590ba3a
Muhammed Anees P P <muhammedaneespp.work@gmail.com>
2024-04-11 01:20:19 +0530

    Showing the records on website, build the table with bootstrap

--- website/views.py
+++ website/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from . forms import SignUpForm
+from . models import Record
 
 
 def home(request):
+    records = Record.objects.all()
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +22,7 @@ def home(request):
             messages.success(request, "There was an error logging in, Please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 # def login_user(request):
